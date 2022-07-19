fa4dd0ab32c87bb25745a4f40c810de07fc4bfac
Vadim <rock2k3@yandex.ru>
2022-07-19 13:57:50 +0300

    add health check handler

--- internal/appV2/server/http-server.go
+++ internal/appV2/server/http-server.go
@@ -3,16 +3,33 @@ package server
 import (
 	"github.com/Rock2k3/notes-core/internal/appV2/config"
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 type httpServer struct {
-	echo *echo.Echo
+	echo   *echo.Echo
+	router *echo.Router
 }
 
 func NewHttpServer() *httpServer {
-	return &httpServer{echo: echo.New()}
+	e := echo.New()
+	return &httpServer{
+		echo:   e,
+		router: e.Router(),
+	}
 }
 
 func (s httpServer) Run(c *config.AppConfig) error {
+	s.configureRouter()
 	return s.echo.Start(c.Http().HttpAddress())
+}
+
+func (s httpServer) configureRouter() {
+	s.router.Add(http.MethodGet, "/health_check", s.HealthCheckHandler())
+}
+
+func (s httpServer) HealthCheckHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Ok")
+	}
 }
